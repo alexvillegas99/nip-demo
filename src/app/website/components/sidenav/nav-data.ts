a8d426d441b5775f3efa4cc2712bcd542ffba3d7
Jonathan Parra <jonathan01.07.95@gmail.com>
2024-08-26 16:41:26 -0500

    listas

--- src/app/website/components/sidenav/nav-data.ts
+++ src/app/website/components/sidenav/nav-data.ts
@@ -1,93 +1,269 @@
-import { INavbarData } from "./helper";
+import { INavbarData } from './helper';
 
 export const NAVBARDATA: INavbarData[] = [
-    {
-        routeLink: './dashboard',
-        icon: 'assets/imgs/navbar/portal-d.svg',
-        label: 'Portal',
-        items: [
-            // {
-            //     routeLink: './administracion-usuarios/gestion-usuarios',
-            //     label: 'Administradores',
-            //     icon: 'assets/imgs/navbar/portal-d.svg',
-            // }
-        ],
-        // roles: ['ADMIN']
-    },
-    {
+  {
+    routeLink: './dashboard',
+    icon: 'assets/imgs/navbar/portal-d.svg',
+    label: 'Portal',
+    items: [
+      // {
+      //     routeLink: './administracion-usuarios/gestion-usuarios',
+      //     label: 'Administradores',
+      //     icon: 'assets/imgs/navbar/portal-d.svg',
+      // }
+    ],
+    // roles: ['ADMIN']
+  },
+  {
+    routeLink: '',
+    icon: 'assets/imgs/navbar/equipos-d.svg',
+    label: 'Monitoreo',
+    items: [
+      {
         routeLink: '',
-        icon: 'assets/imgs/navbar/equipos-d.svg',
-        label: 'Monitoreo',
+        label: 'Maquinaria y Equipos',
+        icon: 'assets/imgs/navbar/maquinaria-d.svg',
         items: [
-            {
-                routeLink: '',
-                label: 'Medidor',
-                icon: 'assets/imgs/navbar/medidor-d.svg',
-                items: [
-                    {
-                        routeLink: './mantenimiento',
-                        label: 'Mantenimiento',
-                        icon: 'assets/imgs/navbar/mantenimiento-d.svg',
-                        items: []
-                    },
-                    {
-                        routeLink: './gestion',
-                        label: 'Gestión',
-                        icon: 'assets/imgs/navbar/reportes-d.svg',
-                        items: []
-                    },
-                ]
-            },
-            {
+          {
+            routeLink: './mantenimiento',
+            label: 'Compresores',
+            icon: '',
+            items: [
+              {
                 routeLink: './reportes',
-                icon: 'assets/imgs/navbar/reporteria-d.svg',
-                label: 'Reportes',
+                icon: '',
+                label: 'Compresor 1',
                 items: [
-                    // {
-                    //     routeLink: './reportes',
-                    //     label: 'Reporte Incidentes',
-                    //     icon: 'assets/imgs/navbar/reporte-incidente-d.svg',
-                    // },
+                  // {
+                  //     routeLink: './reportes',
+                  //     label: 'Reporte Incidentes',
+                  //     icon: 'assets/imgs/navbar/reporte-incidente-d.svg',
+                  // },
                 ],
-                // roles: ['ADMIN', 'BASIC']
-            },
-            // {
-            //     routeLink: '',
-            //     label: 'Variadores',
-            //     icon: 'assets/imgs/navbar/medidor-d.svg',
-            //     items: [
-            //         {
-            //             routeLink: './mantenimiento',
-            //             label: 'Mantenimiento',
-            //             icon: 'assets/imgs/navbar/mantenimiento-d.svg',
-            //         },
-            //         {
-            //             routeLink: './gestion',
-            //             label: 'Gestión',
-            //             icon: 'assets/imgs/navbar/reportes-d.svg',
-            //         }
-            //     ]
-            // },
+              },
+            ],
+          },
+          {
+            routeLink: './gestion',
+            label: 'Variadores',
+            icon: '',
+            items: [
+              {
+                routeLink: './reportes',
+                icon: '',
+                label: 'Variadores 1',
+                items: [
+                  // {
+                  //     routeLink: './reportes',
+                  //     label: 'Reporte Incidentes',
+                  //     icon: 'assets/imgs/navbar/reporte-incidente-d.svg',
+                  // },
+                ],
+              },
+            ],
+          },
         ],
-        // roles: ['ADMIN', 'BASIC']
-    },
-    {
-        routeLink: 'consumo-energetico',
-        icon: 'assets/imgs/navbar/consumo-energetico-d.svg',
-        label: 'Consumo Energético',
+      },
+      {
+        routeLink: '',
+        label: 'Instrumentación y medidores',
+        icon: 'assets/imgs/navbar/inst-medidores-d.svg',
         items: [
-            // {
-            //     routeLink: '',
-            //     label: 'Módulos',
-            // },
+          {
+            routeLink: './mantenimiento',
+            label: 'Sensor de Presión',
+            icon: '',
+            items: [],
+          },
+          {
+            routeLink: './gestion',
+            label: 'Medidores',
+            icon: '',
+            items: [
+              {
+                routeLink: './mantenimiento',
+                label: 'Medidor 1',
+                icon: '',
+                items: [],
+              },
+              {
+                routeLink: './mantenimiento',
+                label: 'Medidor 2',
+                icon: '',
+                items: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        routeLink: '',
+        label: 'Procesos',
+        icon: 'assets/imgs/navbar/procesos.svg',
+        items: [
+          {
+            routeLink: './mantenimiento',
+            label: 'Proceso IP',
+            icon: '',
+            items: [],
+          },
+        ],
+      },
+      {
+        routeLink: './reportes',
+        icon: 'assets/imgs/navbar/reporteria-d.svg',
+        label: 'Reportes',
+        items: [
+          // {
+          //     routeLink: './reportes',
+          //     label: 'Reporte Incidentes',
+          //     icon: 'assets/imgs/navbar/reporte-incidente-d.svg',
+          // },
         ],
         // roles: ['ADMIN', 'BASIC']
-    },
-    {
-        routeLink: './mantenimiento',
-        icon: 'assets/imgs/navbar/configuracion-d.svg',
-        label: 'Configuración',
-        items: [],
+      },
+      // {
+      //     routeLink: '',
+      //     label: 'Variadores',
+      //     icon: 'assets/imgs/navbar/medidor-d.svg',
+      //     items: [
+      //         {
+      //             routeLink: './mantenimiento',
+      //             label: 'Mantenimiento',
+      //             icon: 'assets/imgs/navbar/mantenimiento-d.svg',
+      //         },
+      //         {
+      //             routeLink: './gestion',
+      //             label: 'Gestión',
+      //             icon: 'assets/imgs/navbar/reportes-d.svg',
+      //         }
+      //     ]
+      // },
+    ],
+    // roles: ['ADMIN', 'BASIC']
+  },
+  {
+    routeLink: '',
+    icon: 'assets/imgs/navbar/mantenimiento-d.svg',
+    label: 'Mantenimiento',
+    items: [
+      {
+        routeLink: '',
+        label: 'Gestión de Mantenimiento',
+        icon: 'assets/imgs/navbar/gest-mantenimiento-d.svg',
+        items: [
+          {
+            routeLink: './gestion',
+            label: 'Pedidos',
+            icon: '',
+            items: [],
+          },
+          {
+            routeLink: './gestion',
+            label: 'Tareas',
+            icon: '',
+            items: [],
+          },
+        ],
+      },
+      {
+        routeLink: '',
+        label: 'Maquinaria y Equipos',
+        icon: 'assets/imgs/navbar/maquinaria-d.svg',
+        items: [
+          {
+            routeLink: './mantenimiento',
+            label: 'Compresores',
+            icon: '',
+            items: [
+              {
+                routeLink: './reportes',
+                icon: '',
+                label: 'Compresor 1',
+                items: [
+                  // {
+                  //     routeLink: './reportes',
+                  //     label: 'Reporte Incidentes',
+                  //     icon: 'assets/imgs/navbar/reporte-incidente-d.svg',
+                  // },
+                ],
+              },
+            ],
+          },
+          {
+            routeLink: './gestion',
+            label: 'Variadores',
+            icon: '',
+            items: [
+              {
+                routeLink: './reportes',
+                icon: '',
+                label: 'Variadores 1',
+                items: [
+                  // {
+                  //     routeLink: './reportes',
+                  //     label: 'Reporte Incidentes',
+                  //     icon: 'assets/imgs/navbar/reporte-incidente-d.svg',
+                  // },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        routeLink: '',
+        label: 'Instrumentación y medidores',
+        icon: 'assets/imgs/navbar/inst-medidores-d.svg',
+        items: [
+          {
+            routeLink: './mantenimiento',
+            label: 'Sensor de Presión',
+            icon: '',
+            items: [],
+          },
+          {
+            routeLink: './gestion',
+            label: 'Medidor de Energía',
+            icon: '',
+            items: [],
+          },
+        ],
+      },
+      {
+        routeLink: '',
+        label: 'Procesos',
+        icon: 'assets/imgs/navbar/procesos.svg',
+        items: [
+          {
+            routeLink: './mantenimiento',
+            label: 'Proceso IP',
+            icon: '',
+            items: [],
+          },
+        ],
+      },
+      {
+        routeLink: './reportes',
+        icon: 'assets/imgs/navbar/reporteria-d.svg',
+        label: 'Reportes',
+        items: [
+          // {
+          //     routeLink: './reportes',
+          //     label: 'Reporte Incidentes',
+          //     icon: 'assets/imgs/navbar/reporte-incidente-d.svg',
+          // },
+        ],
         // roles: ['ADMIN', 'BASIC']
-    },
+      },
+    ],
+    // roles: ['ADMIN', 'BASIC']
+  },
+  {
+    routeLink: './mantenimiento',
+    icon: 'assets/imgs/navbar/configuracion-d.svg',
+    label: 'Configuración',
+    items: [],
+    // roles: ['ADMIN', 'BASIC']
+  },
 ];
