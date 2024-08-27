5c4292d5e81cb7a18f68a962e4c353c929fd9102
Alex Villegas <av058554@gmail.com>
2024-08-26 23:41:51 -0500

    componente medidor radial,  responsive menu

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,7 +14,8 @@ import { provideServiceWorker } from '@angular/service-worker';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { TimepickerModule } from 'ngx-bootstrap/timepicker';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { NgxGaugeModule } from 'ngx-gauge';
+import { LinearGaugeModule } from '@syncfusion/ej2-angular-lineargauge';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
@@ -29,5 +30,7 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(TimepickerModule.forRoot()),
     importProvidersFrom(NgbModule),
     importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(NgxGaugeModule),
+    importProvidersFrom(LinearGaugeModule)
   ],
 };
